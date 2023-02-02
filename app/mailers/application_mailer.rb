cf619848ab6849a435610a88817f089525c80304
DDMeer <passionclam@gmail.com>
2023-02-02 01:46:18 -0800

    granerate app info

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  # default from: 'hello@corsego.com'
+  default from: 'Corsego <hello@corsego.com>'
+  layout 'mailer'
 end
